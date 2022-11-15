c9a20206a15a78eb13a65ffbb493e6091732a94a
Влад <@DESKTOP-P1V902B>
2022-11-15 21:45:14 +0500

    Добавьте файлы проекта.

---
+++ Pz_12.2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pz_12._2
+{
+    internal class Program
+    {
+            static void Main(string[] args)
+        {
+            int i = 1;
+            while (i <= 3)
+            {
+                i++;
+
+                Console.WriteLine("Введите значение a");
+                int a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите значение h");
+                int h = Convert.ToInt32(Console.ReadLine());
+
+
+                TriangleP(a, h);
+
+            }
+            Console.ReadKey();
+
+        }
+        static void TriangleP(double a, double h)
+        {
+            double b = Math.Sqrt(Math.Pow(a / 2, 2) + Math.Pow(h, 2));
+            double Perimetr = 2 * b + a;
+            Console.WriteLine($" периметр: {Perimetr}");
+
+
+        }
+    }
+}
+
+
+        
+    
+
+    
+
---
+++ pz_12/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int i = 1;
+        while (i <= 3)
+        {
+            i++;
+
+            Console.WriteLine("Введите значение a");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение h");
+            int h = Convert.ToInt32(Console.ReadLine());
+
+
+            TriangleP(a, h);
+
+        }
+        Console.ReadKey();
+
+    }
+    static void TriangleP(double a, double h)
+    {
+        double b = Math.Sqrt(Math.Pow(a / 2, 2) + Math.Pow(h, 2));
+        double Perimetr = 2 * b + a;
+        Console.WriteLine($" периметр: {Perimetr}");
+
+
+    }
+
+}
+
