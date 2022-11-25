a290d5ff6b7ac0847222b0bbf1165de37b9a61d7
Craqteroc <118295184+Craqteroc@users.noreply.github.com>
2022-11-25 22:19:21 +0500

    Add files via upload

---
+++ pz_15/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace pz_15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string f1 = File.ReadAllText(@"D:text1.txt");
+            string f2 = File.ReadAllText(@"D:text2.txt");
+            
+            File.WriteAllText(@"D:text3.txt", f1);
+            
+            File.WriteAllText(@"D:text2.txt",
+                File.ReadAllText(@"D:text3.txt"));
+            
+            File.WriteAllText(@"D:text1.txt", f2);
+            Console.ReadKey();
+        }
+    }
+}
