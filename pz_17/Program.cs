f34a290f5567aa99aac42b3de6808c6c64e8b897
Craqteroc <118295184+Craqteroc@users.noreply.github.com>
2022-12-12 23:21:30 +0500

    Add files via upload

---
+++ pz_17/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_17
+{
+    internal class Program
+    {
+        unsafe static void Main(string[] args)
+        {
+
+            int*[] y = new int*[2];
+            Console.WriteLine("Введите первое число");
+            int c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            int v = Convert.ToInt32(Console.ReadLine());
+
+            y[0] = &c;
+            y[1] = &v;
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine($"в ячейке по адресу {(uint)y[i]} хранится значение: {(uint)*y[i]}");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
