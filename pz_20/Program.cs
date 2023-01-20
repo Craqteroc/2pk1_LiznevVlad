a1f4d98789235743ed0357d7eef3ed8d92dc6fb3
Craqteroc <118295184+Craqteroc@users.noreply.github.com>
2023-01-21 01:55:22 +0500

    Add files via upload

---
+++ pz_20/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_20
+{
+  
+    internal class Program
+    {
+        static void stringULTRA(ref string data)
+        {
+            char[] rawData = data.ToCharArray();
+            for (int i = 0; i < rawData.Length; i++)
+            {
+                if (char.IsLetter(rawData[i]))
+                    rawData[i] = char.IsUpper(rawData[i]) ? char.ToLower(rawData[i]) : char.ToUpper(rawData[i]);
+            }
+            data = new string(rawData);
+        }
+        static void Main(string[] args)
+        {
+            string data = "";
+            data = Console.ReadLine();
+            stringULTRA(ref data);
+            Console.WriteLine(data);
+            Console.ReadKey();
+        }
+    }
+    //class Person
+    //    {
+        
+    //    private int Data;
+    //    public string Name { get; set; }
+    //    public int data
+    //    {
+            
+    //        get { return Data; }------------------------------------------------ОЧЕНЬ СЫРОЕ ИСКЛЮЧЕНИЕ
+    //        set
+    //        {
+    //            if (value == )
+    //            {
+    //                throw new Exception("Ввели неверное предложение");
+    //            }
+    //            else
+    //            {
+    //                Data = value;
+    //            }
+    //        }
+    //    }
+
+    //    }
+}
